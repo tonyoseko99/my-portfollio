3cb34ce83f50d379a05e536efe327a87a1eca0ce
Tonny Oseko <tonnyoseko0@gmail.com>
2022-10-30 22:41:58 +0300

    add Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Nabar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Nabar />
+      
       
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Nabar() {
+  return (
+    <header className="header">
+      <nav class="nav">
+        <a class="nav-link active" href="#">
+          Home
+        </a>
+        <a class="nav-link" href="#">
+          About Me
+        </a>
+        <a class="nav-link" href="#">
+          Skills
+        </a>
+        <a class="nav-link" href="#">
+          Projects
+        </a>
+        <a class="nav-link" href="#">
+          Contact Me
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Nabar;
