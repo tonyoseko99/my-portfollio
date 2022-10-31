bca0409247dd694e38983a0b0bf5f752f1fddd53
Tonny Oseko <tonnyoseko0@gmail.com>
2022-10-31 20:37:22 +0300

    add project section and style it

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,17 @@
 import React from "react";
-import image from "../assets/image.png";
 import Project from "./Project";
+import About from "./About";
 
 function Home() {
   return (
-    <>
+    <div className="container">
       {/* about section */}
-      <section className="about" id="about">
-        <div className="container">
-          <div className="about__container">
-            <h2 className="about__title">ABOUT ME</h2>
-            <p className="about__text" id="about-text-1">Web designer and web developer</p>
-
-            <p className="about__text">
-              I'm a front-end web developer, JavaScript programmer, and React.js
-              developer. I love making websites look beautiful and
-              user-friendly. I also love playing around with new technologies to
-              see how they can be used in different ways.
-            </p>
-            <button type="button" class="btn btn-dark btn-lg">
-              Download CV <i class="fas fa-download"></i>
-            </button>
-          </div>
-          <img src={image} alt="about" className="about__img" />
-        </div>
-      </section>
+      <About />
       {/* end of about section */}
-      {/* projects section */}
-      <section className="container"></section>
-    </>
+      {/* project section */}
+      <Project />
+      {/* end of project section */}
+    </div>
   );
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,19 +2,89 @@ import React from "react";
 
 function Project() {
   return (
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="..." alt="Card image cap"></img>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+    <section className="project" id="project">
+      <div className="container">
+        <h2 className="project__title">PROJECTS</h2>
+        <div className="project__container">
+            {/* project-1 */}
+          <div className="project__card">
+            <div className="project__card--img">
+              <img
+                src="https://images.unsplash.com/photo-1610392019363-8b1b2b2b2b1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                alt="project"
+              />
+            </div>
+            <div className="project__card--content">
+              <h3 className="project__card--title">Project 1</h3>
+              <p className="project__card--text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quae.
+              </p>
+              <div className="project__card--btn">
+                <a href="#" className="btn btn-dark">
+                  View Project
+                </a>
+                <a href="#" className="btn btn-dark">
+                  View Code
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* end of project-1 */}
+            {/* project-2 */}
+            <div className="project__card">
+            <div className="project__card--img">
+              <img
+                src="https://images.unsplash.com/photo-1610392019363-8b1b2b2b2b1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                alt="project"
+              />
+            </div>
+            <div className="project__card--content">
+              <h3 className="project__card--title">Project 1</h3>
+              <p className="project__card--text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quae.
+              </p>
+              <div className="project__card--btn">
+                <a href="#" className="btn btn-dark">
+                  View Project
+                </a>
+                <a href="#" className="btn btn-dark">
+                  View Code
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* end of project-2 */}
+            {/* project-3 */}
+            <div className="project__card">
+            <div className="project__card--img">
+              <img
+                src="https://images.unsplash.com/photo-1610392019363-8b1b2b2b2b1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                alt="project"
+              />
+            </div>
+            <div className="project__card--content">
+              <h3 className="project__card--title">Project 1</h3>
+              <p className="project__card--text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quae.
+              </p>
+              <div className="project__card--btn">
+                <a href="#" className="btn btn-dark">
+                  View Project
+                </a>
+                <a href="#" className="btn btn-dark">
+                  View Code
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* end of project-3 */}
+            
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
